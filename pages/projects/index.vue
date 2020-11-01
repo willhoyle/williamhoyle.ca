b7df5712226ed9d7a739d782fbf6d6d483b09125
Will Hoyle <will.hoyle@gmail.com>
2020-11-01 00:22:35 -0400

    Move teafile to projects

---
+++ pages/projects/index.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+    div
+        div.text-4xl.text-gray-800 Projects
+        div.flex.flex-wrap
+            div.rounded.border.shadow.p-5
+                div.text-xl.text-gray-800 
+                    nuxt-link(to="/projects/teafile") teafile.js
+                div.text-gray-800 javascript library for teafile format to store time series as binary flat files
+            
+</template>
---
+++ pages/projects/teafile.vue
@@ -0,0 +1,15 @@
+<script>
+import teafile from '../../content/projects/teafile.md'
+
+export default {
+  layout(context) {
+    return 'default'
+  },
+
+  created() {},
+  render: function(createElement) {
+    console.log(teafile)
+    return createElement(teafile.vue.component, [])
+  }
+}
+</script>
