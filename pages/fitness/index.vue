5ba44604efdd0cdf5c4c61facaf871201aa1e2e4
Will Hoyle <will.hoyle@gmail.com>
2020-12-02 23:02:17 -0500

    Start work on Strava training log clone

---
+++ pages/fitness/index.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+div
+  .text-4xl.text-gray-800.mb-5 Fitness
+  .flex.flex-wrap
+    .w-full.rounded(class='md:w-1/2')
+      .text-xl.text-gray-800 
+        .front-page-links activity log
+      .text-gray-800
+</template>
