39d5a3e4fecf91a056dc03fbf68df8a9085e72f8
Will Hoyle <will.hoyle@gmail.com>
2020-12-05 12:22:07 -0500

    page(new) notes page

---
+++ components/PageLayout.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+div
+  .flex
+    .flex-grow.text-4xl.text-gray-800 {{ title }}
+    .flex-grow.text-right.text-lg.text-gray-600 {{ updatedAt }}
+  .text-lg.text-gray-600 {{ subtitle }}
+  slot(name='body')
+</template>
+
+<script>
+export default {
+  props: {
+    attributes: {
+      type: Object,
+      default: Object,
+    },
+  },
+  computed: {
+    title() {
+      return this.attributes.title
+    },
+    createdAt() {
+      return this.attributes.createdAt
+    },
+    updatedAt() {
+      return this.attributes.updatedAt
+    },
+    subtitle() {
+      return this.attributes.subtitle
+    },
+  },
+}
+</script>
